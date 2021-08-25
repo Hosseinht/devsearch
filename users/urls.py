6349f69ac312b948bf2862e0a2679fb58436adc1
Ryan Taheri <sailantht@gmail.com>
2021-08-25 16:38:14 +0430

    3.Render Profile

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('profile/<str:pk>/', views.user_profile, name="user-profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 def profile(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
 
+
+def user_profile(request, pk):
+    userprofile = Profile.objects.get(id=pk)
+
+    top_skills = userprofile.skill_set.exclude(description__exact='')
+    other_skills = userprofile.skill_set.filter(description='')
+
+    context = {'userprofile': userprofile, 'top_skills': top_skills, 'other_skills': other_skills}
+    return render(request, 'users/user_profile.html', context)
